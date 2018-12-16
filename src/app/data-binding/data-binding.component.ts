78e308b6d2e9a883a56e3a4ff4b2cb3aee964010
tonhodofus <marcospad92@gmail.com>
2018-12-16 17:09:15 -0200

    two-way data binding
    ..

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,11 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  styles:[
+    `
+    .highlight {
+      background-color: yellow;
+      font-weight: bold;
+    }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url = 'http://loiane.com';
+  conteudoAtual : String = '';
+  conteudoSalvo : String = '';
+  isMouseOver = false;
+  nome = 'abc';
+  pessoa = {
+    nome : 'def',
+    idade : 20
+  }
+
+  onClick(){
+    alert ("Alô Alô Marciano!");
+  }
+
+  onKeyup(event){
+    console.log(event);
+    this.conteudoAtual = event.target.value;
+  }
+
+  onSave(valor : String){
+    this.conteudoSalvo = valor;
+  }
+
+  onMouseSpan (){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
 
   getValor(){
     return 2;
